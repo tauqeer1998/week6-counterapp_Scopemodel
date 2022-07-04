5ad53fb645c926daf34952a5d076c9365dbbc9f0
Muhammad Tauqeer <50796565+tauqeer1998@users.noreply.github.com>
2022-07-04 17:37:17 +0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/scopemodel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scopemodel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'CounterModel.dart';
+
+void main() {
+  runApp( MyApp(
+    model: CounterModel(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final CounterModel model;
+
+  const MyApp({Key? key, required this.model}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<CounterModel>(
+      model: model,
+      child: MaterialApp(
+        home: CounterHome('Counter App'),
+      ),
+    );
+  }
+}
+class CounterHome extends StatelessWidget {
+  final String title;
+
+  CounterHome(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            ScopedModelDescendant<CounterModel>(
+              builder: (context, child, model) {
+                return Text(
+                  model.counter.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: ScopedModelDescendant<CounterModel>(
+        builder: (context, child, model) {
+          return FloatingActionButton(
+            onPressed: model.increment,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          );
+        },
+      ),
+    );
+  }
+}
